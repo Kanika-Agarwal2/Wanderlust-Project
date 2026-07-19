ab3bfed8946b864fb2b5d98e58f25886d8058abd
Kanika Agarwal <kanikagarwal.1902@gmail.com>
2026-07-19 22:01:34 +0530

    Redirect root to listings

--- app.js
+++ app.js
@@ -65,7 +65,9 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
--- init/index.js
+++ init/index.js
@@ -1,15 +1,18 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config({ path: "../.env" });
+}
 const mongoose = require("mongoose");
-const Mongo_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const Listing = require("../models/listing.js")
-const initData = require("./data.js")
+const initData = require("./data.js");
+const dbURL = process.env.ATLASDB_URL;
 main().then((res) => console.log("connection successful"))
     .catch((err) => console.log(err));
 async function main() {
-    await mongoose.connect(Mongo_URL);
+    await mongoose.connect(dbURL);
 }
 const initDb = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({...obj, owner : '6a590268f44d85c1224046e2'}));
+    initData.data = initData.data.map((obj) => ({...obj, owner : "6a5ca4879cfccb5230f56144"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
